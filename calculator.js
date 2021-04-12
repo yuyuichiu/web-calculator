dd3a0f7e29cd785716dd6a4da2d7a75ee8b1a1ba
MaxwellYu <yuichiuyu1915@gmail.com>
2021-04-12 22:29:56 +0800

    .

--- calculator.js
+++ calculator.js
@@ -5,37 +5,55 @@ function loadDone(){
     loadScreen = document.getElementById("loading");
     loadScreen.style.visibility = "hidden";
     loadScreen.style.opacity = "0";
+    loadScreen.style.height = "0";
 }
 
 // Button click inputs
 function calWrite(order){
-    calInput = document.getElementById("calInput");
+    let calInput = document.getElementById("calInput");
     calInput.value += order;
 }
 
+// Clear inputs
 function clearInput(){
-    calInput = document.getElementById("calInput");
+    let calInput = document.getElementById("calInput");
+    let calOutput = document.getElementById("calOutput");
     calInput.value = "";
+    calOutput.innerText = "0";
+    calOutput.style.color = "rgb(44, 44, 44)";
 }
 
 // Calculation
 function calExecute(raw){
     var raw = document.getElementById("calInput").value;
     var calOutput = document.getElementById("calOutput");
-    // Translate Symbols to math commands
-    raw = raw.replace("x","*");
-    raw = raw.replace("÷","/");
+    calOutput.style.color = "rgb(44, 44, 44)";
+    // Translate Symbols to operators
+    raw = raw.replace(/[x]/g,"*");
+    raw = raw.replace(/[÷]/g,"/");
     // Error Detection
-    if(raw == ""){ return }; // do nothing when input is null
+    if(raw == ""){ return }; // output nothing when input is null
 
-    // Check if input does not contain unexpected input
-    // Proceed only with valid input to avoid eval() being used maliciously
-    if(!(/^[\+\-\*\/0-9]+$/.test(raw))){
-        calOutput.innerText = "ERROR";
+    // Input validity check to avoid eval() being used maliciously
+    // 1. Reject input with invalid characters
+    if(!(/^[\+\-\*\/0-9.]+$/.test(raw))){
+        calOutput.innerText = "Syntax Error";
         return
     };
 
-    calOutput.innerText = eval(raw);
+    result = eval(raw);
+    if (result > 1000000000000 || result < -1000000000000){
+        calOutput.innerText = "Overflow";
+        return
+    }
+    else{
+        calOutput.innerText = result;
+    }
+
+    // Negative Value Styling
+    if (result < 0){
+        calOutput.style.color = "rgb(253, 71, 71)";
+    }
 }
 
 // calExecute("8 / 2 + 3 * 4 - 6");
